ca942df995e16e8b99fd8e03277aa6cd8f3c1d99
Tbetcha <troy.boettger@gmail.com>
2020-11-14 01:23:59 -0500

    Update deals page to reflect more restrictive query params

--- deals.aspx.cs
+++ deals.aspx.cs
@@ -18,10 +18,15 @@ namespace taketwo
                 string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\troyboettger\source\repos\taketwo\App_Data\Carshop.accdb;Persist Security Info=True;";
 
 
+                string SqlString = "SELECT DISTINCT Type FROM Sales WHERE TopDeal = (?)";
+                var deal = new OleDbParameter("@TopDeal", OleDbType.VarChar);
+                deal.Value = "Y";
+
                 using (OleDbConnection con = new OleDbConnection(connectionString))
                 {
                     //Open Database Connection
-                    OleDbDataAdapter da = new OleDbDataAdapter("SELECT DISTINCT [Type] FROM Sales", con);
+                    OleDbDataAdapter da = new OleDbDataAdapter(SqlString, con);
+                    da.SelectCommand.Parameters.Add(deal);
 
                     DataSet ds = new DataSet();
 
